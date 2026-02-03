cda0e4b1847aebfc5aeba2b98dd32eaaf8b1a40f
Victor <vicnfgames@gmail.com>
2026-02-03 14:38:28 -0300

    Adicionado um arquivo separado para acessos à Api

--- src/Componentes/Forms/Input.jsx
+++ src/Componentes/Forms/Input.jsx
@@ -6,7 +6,7 @@ function Input(props)
 {
     return <div class={estilos.formContainer}>
         <label class={estilos.label} for={props.name}>{props.label}</label>
-        <input {...props} id={props.name} class={estilos.input} type={props.type} name={props.name} valor={props.valor} onChange={props.onChange} onBlur={props.onBlur}></input>
+        <input id={props.name} class={estilos.input} type={props.type} name={props.name} valor={props.valor} onChange={props.onChange} onBlur={props.onBlur}></input>
         {props.erro ? <p class={estilos.erro}>{props.erro}</p> : ""}
     </div>
 
--- src/Componentes/Login/LoginForm.jsx
+++ src/Componentes/Login/LoginForm.jsx
@@ -4,33 +4,43 @@ import {Link} from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
+import {TOKEN_POST, USER_GET} from "../../api";
 
 function LoginForm()
 {
     const usuario = useForm();
     const senha = useForm();
 
-    function handleSubmit(event)
+    React.useEffect(() => 
+    {
+        const token = window.localStorage.getItem("token");
+        if(token)
+            getUsuario(token);
+
+    }, []);
+
+    async function getUsuario(token)
+    {
+        const {url, options} = USER_GET(token);
+
+        const result = await fetch(url, options);
+        const dados = await result.json();
+        console.log(dados);
+
+    }
+
+    async function handleSubmit(event)
     {
         event.preventDefault();
         if(usuario.validar() && senha.validar())
         {
-            fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token",
-            {
-                method: "POST",
-                headers:
-                {
-                    "Content-Type": "application/json"
+            const {url, options} = TOKEN_POST({username: usuario.value, password: senha.value});
 
-                },
-                body: JSON.stringify({usuario, senha})
-                
-            }).then((result) =>
-            {
-                console.log(result);
-                return result.json();
-                
-            }).then((dados) => console.log(dados));
+            const result = await fetch(url, options);
+            const dados = await result.json();
+            window.localStorage.setItem("token", dados.token);
+            getUsuario(dados.token);
+            console.log(dados);
 
         }
 
