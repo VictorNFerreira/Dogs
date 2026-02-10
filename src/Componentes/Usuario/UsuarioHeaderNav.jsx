73a730c428f19f32c3fa477874898fc9657866ea
Victor <vicnfgames@gmail.com>
2026-02-10 14:13:46 -0300

    Adicionado o custom hook useMedia e um menu mobile para o Header da página de usuário

--- src/Componentes/Usuario/UsuarioHeaderNav.jsx
+++ src/Componentes/Usuario/UsuarioHeaderNav.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {NavLink, useLocation} from "react-router-dom";
 
 import {UserContext} from "../../UserContext";
+import useMedia from "../../Hooks/useMedia";
 
 import estilos from "../../css/usuario/UsuarioHeaderNav.module.css"
 import Feed from "../../assets/feed.svg?react";
@@ -11,19 +12,31 @@ import Sair from "../../assets/sair.svg?react";
 
 function UsuarioHeaderNav()
 {
-    const [mobile, setMobile] = React.useState(null);
     const {usuarioLogout} = React.useContext(UserContext);
+    const mobile = useMedia("(max-width: 800px)");
+    const [menuMobile, setMenuMobile] = React.useState(false);
+    const {pathname} = useLocation();
 
-    return <nav class={estilos.nav}>
-        <ul>
-            <li><NavLink to="/conta" end><Feed/> {mobile ? "Minhas Fotos" : ""}</NavLink></li>
-            <li><NavLink to="/conta/postar"><Adicionar/> {mobile ? "Adicionar Foto" : ""}</NavLink></li>
-            <li><NavLink to="/conta/estatisticas"><Estatisticas/> {mobile ? "Estatísticas" : ""}</NavLink></li>
-        </ul>
+    React.useEffect(() =>
+    {
+        setMenuMobile(false);
 
-        <button class={estilos.sair} onClick={usuarioLogout}><Sair/> {mobile ? "Sair" : ""}</button>
-    </nav>
+    }, [pathname]);
 
+    return <>
+        {mobile ? <button class={`${estilos.botaoMobile} ${menuMobile ? estilos.botaoMobileAtivo : ""}`} onClick={() => setMenuMobile(!menuMobile)}></button> : ""}
+
+        <nav class={`${mobile ? estilos.navMobile : estilos.nav} ${menuMobile ? estilos.navMobileAtivo : ""}`}>
+            <ul>
+                <li><NavLink to="/conta" end><Feed/> {mobile ? "Minhas Fotos" : ""}</NavLink></li>
+                <li><NavLink to="/conta/postar"><Adicionar/> {mobile ? "Adicionar Foto" : ""}</NavLink></li>
+                <li><NavLink to="/conta/estatisticas"><Estatisticas/> {mobile ? "Estatísticas" : ""}</NavLink></li>
+            </ul>
+
+            <button class={estilos.sair} onClick={usuarioLogout}><Sair/> {mobile ? "Sair" : ""}</button>
+        </nav>
+    </>
+    
 }
 
 export default UsuarioHeaderNav;
---
+++ src/Hooks/useMedia.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function useMedia(media)
+{
+    const [match, setMatch] = React.useState(null);
+
+    React.useEffect(() =>
+    {
+        function changeMatch()
+        {
+            const {matches} = window.matchMedia(media);
+            setMatch(matches);
+
+        }
+
+        window.addEventListener("resize", changeMatch);
+        changeMatch();
+
+        return () => {window.removeEventListener("resize", changeMatch)};
+
+    }, [media]);
+
+    return match;
+
+}
+
+export default useMedia;
