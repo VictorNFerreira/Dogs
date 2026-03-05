0bfa21dfbe943de32e5ef326ff530053c35b4799
Victor <vicnfgames@gmail.com>
2026-03-05 12:27:34 -0300

    Adicionado a estrutura para fazer deploy do projeto no github-pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import svgr from "vite-plugin-svgr";
 //https://vite.dev/config/
 export default defineConfig
 (
-    {plugins: [react(), svgr()]}
+    {
+        plugins: [react(), svgr()],
+        base: "/Dogs/",
+
+    }
     
 );
